a75bf0b7b827fe52ca083ba249a0ed05504d3a66
Zare64 <hrh.mc.acct@gmail.com>
2025-04-04 12:59:50 -0400

    Added Player Arsenal/Bullet System

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+from arsenal import ShipArsenal
 class AlienInvasion:
     
     def __init__(self) -> None:
@@ -19,22 +19,62 @@ class AlienInvasion:
         self.running = True
         self.clock= pygame.time.Clock()
 
-        self.ship = Ship(self)
+        self.ship = Ship(self, ShipArsenal(self))
+
+        pygame.mixer.init()
+        self.bullet_sound = pygame.mixer.Sound(self.settings.bullet_sound)
+        self.bullet_sound.set_volume(self.settings.bullet_volume)
+
+
 
     def run_game(self):
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running=False
-                    pygame.quit()
-                    sys.exit()
-            
-            self.screen.blit(self.bg, (0,0))
-            self.ship.draw()
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(self.settings.FPS)
 
 
+    def _update_screen(self):
+        self.screen.blit(self.bg, (0,0))
+        self.ship.draw()
+        pygame.display.flip()
+
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running=False
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event) -> None:
+        if event.key ==pygame.K_d:
+            self.ship.moving_right=True
+        if event.key ==pygame.K_a:
+            self.ship.moving_left=True
+        if event.key == pygame.K_q:
+            self.running=False
+            pygame.quit()
+            sys.exit()
+        if event.key == pygame.K_SPACE:
+            if self.ship.fire():
+                self.bullet_sound.play()
+                self.bullet_sound.fadeout(self.settings.bullet_fadeout)
+
+
+    def _check_keyup_events(self, event) -> None:
+        if event.key ==pygame.K_d:
+            self.ship.moving_right=False
+        elif event.key ==pygame.K_a:
+            self.ship.moving_left=False
+ 
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -9,8 +9,18 @@ class Settings:
         self.FPS = 60
         print(f"My Path is: {Path.cwd()}")
         self.image_import_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'images'
-
+        self.sound_import_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'sound'
+        
         self.bg_file = self.image_import_file / 'Starbasesnow.png'
         self.ship_file = self.image_import_file / 'ship2(no bg).png'
         self.ship_w = 40
         self.ship_h = 60
+
+        self.bullet_file = self.image_import_file / 'laserBlast.png'
+        self.bullet_sound = self.sound_import_file / 'laser.mp3'
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_amount = 5
+        self.bullet_speed = 5
+        self.bullet_volume = 0.7
+        self.bullet_fadeout = 250
