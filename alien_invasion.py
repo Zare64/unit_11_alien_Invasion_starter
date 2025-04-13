0cbbd5bef7b3f321a4998d4d60e72b9b128a8f66
Zare64 <hrh.mc.acct@gmail.com>
2025-04-12 22:05:39 -0400

    Added Reset Level Upon Fleet Destruction

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,7 +66,8 @@ class AlienInvasion:
         #check collisions for aliens and bottom of screen
         if self.alien_fleet.check_fleet_bottom():
             self._reset_level()
-
+        if self.alien_fleet.check_destroyed_status():
+            self._reset_level()
         #check collisions of projectiles and aliens
         collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
         if collisions:
